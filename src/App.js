29722738dba8fe497a0dc74849650725dc34e27e
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-09-01 15:16:15 +0530

    initial files , initial structure added

---
+++ src/App.js
@@ -0,0 +1,45 @@
+function App() {
+  return (
+    <>
+      <div className="categories-container">
+        <div className="category-container">
+          {/*<img />*/}
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/*<img />*/}
+          <div className="category-body-container">
+            <h2>Jackets</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/*<img />*/}
+          <div className="category-body-container">
+            <h2>Sneakers</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/*<img />*/}
+          <div className="category-body-container">
+            <h2>Women</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/*<img />*/}
+          <div className="category-body-container">
+            <h2>Men</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
