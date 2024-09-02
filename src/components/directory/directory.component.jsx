d14fb26443ac1ce73eeee6efdcd3211f25fae428
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-09-02 14:14:56 +0530

    Built directory component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import "./directory.styles.scss";
+//import "../category-item/category-item.styles.scss";
+import CategoryItem from "../category-item/category-item.component";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem category={category} key={category.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
